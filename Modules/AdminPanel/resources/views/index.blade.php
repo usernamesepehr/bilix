0259c496b6ab29d5066a2108fa18cc2baa3962ae
usernamesepehr <usernamesepehr@gmail.com>
2025-12-15 00:54:29 +0330

    remove: adminPanel module removed

--- Modules/AdminPanel/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-adminpanel::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('adminpanel.name') !!}</p>
-</x-adminpanel::layouts.master>
--- Modules/AdminPanel/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\AdminPanel\Http\Controllers\AdminPanelController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('adminpanels', AdminPanelController::class)->names('adminpanel');
-});
